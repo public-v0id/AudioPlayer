ba959b301c957a5f4cd248cad8387c8e5e9fdf8e
PetrIgnatov <petrignatovefr@gmail.com>
2025-04-15 12:37:18 +0300

    Added logo, new visualisation mode, fixed some minor bugs

---
+++ README.md
@@ -0,0 +1,13 @@
+# AudioPlayer
+## Made by public_v0id
+##### A simple audio player for Linux terminal with visualisation
+### Installation
+For installation, copy this repository and type "make install"
+### Usage
+If you want to run a single audiofile, type "player *path*"
+If you want to run a playlist, type "player --playlist=*path*"
+Playlists are just text files with paths to all tracks
+If you want to shuffle tracks in a playlist (and make it infinite), use the "-r" key
+If you want to use WinAmp-like visualisation, use the "-c" key
+If you wnat to use your own ASCII logo file, type "--logo=*path*"
+###### St. Petersburg, 2025, No rights reserved
--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include <complex.h>
 #include <pthread.h>
 #include <stdbool.h>
+#include <errno.h>
 
 #define TABLE_SIZE 200
 #define PI (3.14159265)
@@ -46,25 +47,91 @@ static int outputCallback(const void *inputBuffer, void *outputBuffer, unsigned 
 
 typedef struct {
 	unsigned long framesPerBuffer;
+	uint8_t mode;			//0 for winAmp-like, 1 for cool logo
 	void* userData;
+	char* logoFile;
+	char** logoText;
+	int* x;
+	int* y;
+	int h;
 } graphArgs;
 
 void* graphOut(void *args) {
 	graphArgs *grargs = (graphArgs*)args;
+	printf("%d channels\n", ((audioData*)(grargs->userData))->sfinfo.channels);
+	double ang = M_PI/4;
+	int w, h;
+	getmaxyx(stdscr, h, w);
+	grargs->h = h;
+	FILE* logo = fopen(grargs->logoFile, "r");
+	grargs->logoText = malloc(h*sizeof(char*));
+	char** logoText = grargs->logoText;
+	for (int i = 0; i < h; ++i) {
+		logoText[i] = malloc(sizeof(char)*w);	
+	}
+	int line = 0;
+	int cnt = 0;
+	int sz = 0;
+	if (logo) {
+		while (fgets(logoText[line], w, logo)) {
+			for (int i = 0; i < w; ++i) {
+				if (logoText[line][i] != ' ' && logoText[line][i] != 0) {
+					++cnt;
+					if (sz < i+1) {
+						sz = i+1;
+					}
+				}
+			}
+			++line;
+		}
+		grargs->x = malloc(sizeof(int)*cnt);
+		grargs->y = malloc(sizeof(int)*cnt);
+		int cur_num = 0;
+		for (int i = 0; i < line; ++i) {
+			for (int j = 0; j < sz; ++j) {
+				if (logoText[i][j] != ' ' && logoText[i][j] != 0) {
+					grargs->x[cur_num] = j-sz/2;
+					grargs->y[cur_num] = i-line/2;
+					++cur_num;
+				}
+			}
+		}
+	}
+	printf("%d %d %d", line, cnt, sz);
+	int stdist = 3*sz/2;
+	int mid_h = h/2;
+	int mid_w = w/2;
+	struct timespec rawtime;
+	clock_gettime(CLOCK_MONOTONIC_RAW, &rawtime);
 	while (true) {
 		if (paused) {
 			continue;
 		}
 		float *rptr = ((audioData*)(grargs->userData))->buffer;	
 		clear();
+		int32_t intensity = 0;
 		for (size_t k = 0; k < acc/2; ++k) {
 			float complex im = CMPLX(0,1);
 			float complex x = 0;	
-			for (int i = 0; i < 8*grargs->framesPerBuffer; ++i) {
-				x += rptr[i]*cexpf(-2.0f*im*PI*i*k/acc);
+			for (int i = 0; i < ((audioData*)(grargs->userData))->sfinfo.channels*((audioData*)(grargs->userData))->sfinfo.channels*((audioData*)(grargs->userData))->sfinfo.channels*grargs->framesPerBuffer; ++i) {
+				x += rptr[i]*cexpf(-2.0f*im*PI*i*(((1-grargs->mode)*acc/4+k)%(acc/2))/acc);
 			}
 			int h = (int)(sqrt(creal(x)*creal(x)+cimag(x)*cimag(x)));
 			mvaddch(yres-1-h, k, 'O');
+			intensity += h;
+		}
+		if (grargs->mode) {
+			struct timespec curtime;
+			clock_gettime(CLOCK_MONOTONIC_RAW, &curtime);
+			int32_t elapsed = (curtime.tv_sec - rawtime.tv_sec)*1000+(curtime.tv_nsec - rawtime.tv_nsec)/1000000;
+			for (int i = 0; i < cnt; ++i) {
+                        	int x_cur, y_cur;
+                        	x_cur = mid_w+(int)(grargs->x[i]*sin(ang)); 
+                        	y_cur = mid_h+(int)(grargs->y[i]*(stdist-grargs->x[i]*cos(ang))/stdist);
+                        	mvaddch(y_cur, x_cur, '8');
+                	}
+                	ang += elapsed*0.001*intensity/acc;
+			rawtime = curtime;
 		}
 		refresh();
 	}
@@ -76,7 +143,7 @@ PaError openAudioFile(audioData* data, char* name, int ch, PaStream** outStream)
 		return paNoError+1;
 	}
 	data->framesRemaining = data->sfinfo.frames;	
-	return Pa_OpenDefaultStream(outStream, 0, ch, paFloat32, 44100, 256, outputCallback, data);
+	return Pa_OpenDefaultStream(outStream, 0, data->sfinfo.channels, paFloat32, data->sfinfo.samplerate, 256, outputCallback, data);
 }
 
 int main(int argc, char** argv)	{
@@ -86,8 +153,12 @@ int main(int argc, char** argv)	{
 	}
 	srand(time(NULL));
 	char** playlist;
-	size_t songs = 0;
+	size_t songs = 0;	//allocated memory
+	size_t plSize = 0;
 	bool list = false, random = false;
+	graphArgs gr_args;
+	gr_args.mode = 1;
+	gr_args.logoFile = "logo";
 	for (int arg = 1; arg < argc; ++arg) {
 		if (strncmp(argv[arg], "--playlist=", 11) == 0) {
 			list = true;
@@ -101,6 +172,7 @@ int main(int argc, char** argv)	{
 			while (fgets(line, sizeof(line), file)) {
 				++songs;
 			}
+			plSize = songs;
 			playlist = malloc(sizeof(char*)*songs);
 			rewind(file);
 			size_t i = 0;
@@ -116,16 +188,25 @@ int main(int argc, char** argv)	{
 			fclose(file);
 			continue;
 		}
+		if (strncmp(argv[arg], "--logo=", 7) == 0) {
+			gr_args.logoFile = argv[arg]+7;
+			printf(gr_args.logoFile);
+			continue;
+		}
+		if (strncmp(argv[arg], "-c", 2) == 0) {
+			gr_args.mode = 0;
+		}
 		if (strncmp(argv[arg], "-r", 2) == 0) {
 			random = true;
 		}
 	}
 	if (!list) {
 		playlist = malloc(sizeof(char*)*1);
-		playlist[0] = argv[1];
+		playlist[0] = argv[argc-1];
 		songs = 0;				//no need to free stack memory
+		plSize = 1;
 	}
-	if (!list && argc > 2) {
+	if (argc < 2) {
 		fprintf(stderr, "Incorrect usage! If you want to play a single song, type player *songname*\n");
 		for (size_t i = 0; i < songs; ++i) {
 			free(playlist[i]);
@@ -137,6 +218,18 @@ int main(int argc, char** argv)	{
 	audioData data;
 	data.file = NULL;
 	data.buffer = (float*)malloc(256*8*sizeof(float));
+	for (size_t i = 0; i < plSize; ++i) {
+		FILE* audiofile = fopen(playlist[i], "r");
+		if (audiofile == NULL || errno != 0) {
+			fprintf(stderr, "Couldn't open file %s!\n", playlist[i]);
+			for (size_t j = 0; j < songs; ++j) {
+				free(playlist[j]);
+			}
+			free(playlist);
+			return 0;
+		}
+		fclose(audiofile);
+	}
 	if (data.buffer == NULL) {
 		fprintf(stderr, "Couldn't open file!\n");
 		for (size_t i = 0; i < songs; ++i) {
@@ -190,8 +283,7 @@ int main(int argc, char** argv)	{
 	}
 	printf("Opened!\n");
 	initscr();
-	pthread_t graph_id;
-	graphArgs gr_args;
+	pthread_t graph_id;	
 	gr_args.framesPerBuffer = 256;
 	gr_args.userData = &data;
 	pthread_create(&graph_id, NULL, graphOut, &gr_args);
@@ -335,4 +427,8 @@ int main(int argc, char** argv)	{
 		free(playlist[i]);	
 	}
 	free(playlist);
+	for (int i = 0; i < gr_args.h; ++i) {
+		free(gr_args.logoText[i]);
+	}
+	free(gr_args.logoText);
 }
